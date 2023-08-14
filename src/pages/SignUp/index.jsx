2cb76da89d4f640dde235eda05042fdd1803aa41
fernando <fernandonetodeveloper@gmail.com>
2023-08-14 16:25:42 -0300

    feat: SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+
+import logo from "../../assests/logo_maior.png"
+import { Link } from "react-router-dom"
+
 function SignUp() {
+   const [name, setName] = useState("")
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+
    return (
-      <div>
-         <h1>SignUp</h1>
+      <div className="container-center">
+         <div className="login">
+            <div className="login-area">
+               <img src={logo} alt="logo do sistema de chamados" />
+            </div>
+
+            <form>
+               <h1>Criar Conta</h1>
+
+               <input
+                  type="text"
+                  placeholder="Seu nome"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+               />
+
+               <input
+                  type="text"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+               />
+
+               <input
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
+
+               <button type="submit">Criar conta</button>
+            </form>
+            <Link to="/">Já possui uma conta? Faça login</Link>
+         </div>
       </div>
    )
 }

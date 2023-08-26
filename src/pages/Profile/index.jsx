32b3f66ae28782a524d6d307394c00b3adfcf797
fernando <fernandonetodeveloper@gmail.com>
2023-08-26 16:16:53 -0300

    feat: title component

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,20 @@
+import Header from "../../components/Header"
+import Title from "../../components/Title"
+
+import { FiSettings } from "react-icons/fi"
+
+function Profile() {
+   return (
+      <div>
+         <Header />
+         <div className="content">
+            <Title name="Minha conta">
+               <FiSettings size={25} />
+            </Title>
+         </div>
+         <h1>Profile </h1>
+      </div>
+   )
+}
+
+export default Profile
